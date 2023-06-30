5e119dd2eb56d1c0a01c7645a2c5e5db592367c9
Basil shraim <50707017+Baselshreem@users.noreply.github.com>
2023-06-30 15:38:35 +0300

    Update adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -2,10 +2,12 @@ import time
 import random
 
 def print_pause(statment_printer):
+    #this is a function to puse a masge 
     print(statment_printer)
     time.sleep(2)
     
-def intro():
+def description():
+    #this is a func use a puse a masge to description player 
     print_pause("Welcome to the adventure game. Thank you for choosing.\n")
                 
     print_pause("Now you have reached the adventure game. There are two basic options in the game. A menu will be displayed to choose to start.\n")
@@ -16,58 +18,49 @@ def intro():
  
 
 
-def fight(items, option):
+def Start_Play(items, option):
     # Things that happen when the player fights  
-    print_pause("Enter 1 to knock on the door of the house.")
-    print_pause("Enter 2 to peer into the cave.")
+    print_pause("Enter 1 to house or 2 cave.")
     print_pause("What would you like to do?")
-    while True:
-        choice1 = input("(Please enter 1 or 2.)\n")
-        if choice1 == "1":
-            house(items, option)
-            break
-        elif choice1 == "2":
-            cave(items, option)
-            break
+    
+    choice1 = input("(Please enter 1 or 2.)\n")
+    if choice1 == '1':
+        print_pause("\n\n\nThank you Home has been selected to play.\n\n\n")
+        house(items, option)
+         
+    elif choice1 == '2':
+        print_pause("\nThank you cave has been selected to play.")
+        cave(items, option)
+        
         
 def cave(item, option):
-    if "sward" not in item:
-        print_pause("\nThank you cave has been selected to play.")
+    if "mona" not in item:
          
         print_pause("\nUnsuccessful selection, please play again.\n")
     while True:
-      choi = input("Would you like to (1) fight or (2) ""run away?")
+      choi = input("Would you like to (1) start agin  or (2) Repeat_Play ""run away?")
       
       if choi == '1':
           
-           if "sward" in item:
-               print_pause("\nThank you cave has been selected to play.")
+           if "mona" in item:
+               print_pause("\nThank you successful .")
       
       
       
            else:
-              print_pause("\nYou peer cautiously into the cave.")
-              print_pause("\nIt turns out to be only a very small cave.")
-              print_pause("\nYour eye catches a glint of metal behind a "
-                    "rock.")
-              print_pause("\nYou have found the magical Sword of Ogoroth!")
-              print_pause("\nYou discard your silly old dagger and take "
-                    "the sword with you.")
               print_pause("\nGood luck, please try again..\n")
-              item.append("sward")
-              fight(item, option)    
+              item.append("mona")
+              Start_Play(item, option)    
     
     
       if choi == '2':
-        print_pause("\nYou run back into the field. ""\nLuckily,you don't seem to have been ""followed.\n")
-          
-        field()
+        Repeat_Play()
         break
  
     
 def house(items, option):
     # Things that happen to the player in the house
- print_pause("\n\n\nThank you Home has been selected to play.\n\n\n")
+ 
  choise = input("1. Enter through the door\n"
                 "2. Entry from the window\n")
                   
@@ -84,31 +77,34 @@ def house(items, option):
  elif choise == '2':     
    print_pause("game over ")
    print_pause("restart selected another choices,   method game ")
-   fight(items, option)
+   Start_Play(items, option)
  else:
    print_pause("backe playe ")
-   field()
+   Repeat_Play()
  
 
  
-def field():
+def Repeat_Play():
     # Things that happen when the player runs back to the field
-    again = input("Would you like to play again? (y/n)").lower()
-    if again == "y":
+    print_pause("\n.The game is restarting\n")    
+    repeat = input("Would you like to repeat again, please select 1-OK 2-I do not agreen")
+    
+    if repeat == '1':
         print_pause("\n\n\nExcellent! Restarting the game ...\n\n\n")
         play_game()
-    elif again == "n":
-        print_pause("\n\n\nThanks for playing! See you next time.\n\n\n")
+    elif repeat == '2':
+        print_pause("\n\n\n Thanks see you later.\n\n\n")
     else:
-        field()
+        Repeat_Play()
         
  
 
 def play_game():
     items = []
-    option = random.choice(["wicked fairie", "pirate", "dragon","troll","gorgon"])
-    intro()
-    fight(items, option)
+    option = random.choice(["Basil", "Mohamad", "asad","shraime","sile","dina"])
+    description()#function used a print and push a masge on screen to descrip game 
+    Start_Play(items, option)#this is a call start game and tow parmeter the first items and socond is option random 
+
 
 
 play_game()
